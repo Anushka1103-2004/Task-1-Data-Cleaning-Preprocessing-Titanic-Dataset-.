4bcd4af254f2d747b3e20e864f01979169f3453d
Anushka1103-2004 <anushkakhare755@gmail.com>
2025-11-14 22:54:30 +0530

    Create preprocessing.py

---
+++ python src/preprocessing.py
@@ -0,0 +1,18 @@
+
+---
+
+## 📌 Interview Questions Covered  
+1. Types of missing data  
+2. How to handle categorical features  
+3. Normalization vs Standardization  
+4. Detecting outliers  
+5. Importance of preprocessing  
+6. One-hot vs Label Encoding  
+7. Handling imbalanced data  
+8. How preprocessing affects model accuracy  
+
+---
+
+## 👨‍💻 Author  
+Your Name  
+AI & ML Internship — Task 1 Completed
